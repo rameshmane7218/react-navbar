bbfc0572c48555a214bcec221989ba8d86ac0f60
rameshmane7218 <rameshmane7218@gmail.com>
2022-05-19 20:05:53 +0530

    added uuid

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "./Links.module.css";
+import {v4 as uuidv4} from "uuid";
 export const Links = () => {
   const links = ["Services", "Projects", "About"];
   return (
     <div className={style.linksDiv}>
       {links.map((link,index) => (
-        <div key={index}>{link}</div>
+        <div key={uuidv4()}>{link}</div>
       ))}
     </div>
   );
